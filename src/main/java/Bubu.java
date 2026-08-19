8fdca1552092d0396eae5818af48fd9e174032b2
Zhou Xinyu <e1525502@u.nus.edu>
2026-08-20 01:21:32 +0800

    Level 1 - Implement Echo

--- src/main/java/Bubu.java
+++ src/main/java/Bubu.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class Bubu {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "___________________________________________________________";
+        String meow = " meow~";
         String banner = " /\\___/\\ \n"
                 + "(  o.o  )  Hello! I'm BUBU!\n";
+
         System.out.println(line);
         System.out.println(banner);
         System.out.println("What can I do for you? Meow!");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon! Meow!");
-        System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon! Meow!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(input + meow);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
