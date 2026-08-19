a7ea39894435c69128dd860b44192e1c131c8b97
Zhou Xinyu <e1525502@u.nus.edu>
2026-08-20 05:31:15 +0800

    Level 3 - Implement Mark as Done (fix minor errors)

--- src/main/java/Bubu.java
+++ src/main/java/Bubu.java
@@ -48,7 +48,7 @@ public class Bubu {
         scanner.close();
     }
 
-    public void commandList() {
+    private void commandList() {
         System.out.println("Meow! Here are the tasks in your list:");
         for (int i = 0; i < this.tasks.size(); i++) {
             System.out.println((i + 1) + ". " + this.tasks.get(i));
@@ -56,7 +56,7 @@ public class Bubu {
         System.out.println(line);
     }
 
-    public void commandMark(String input) {
+    private void commandMark(String input) {
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         this.tasks.get(index).markAsDone();
         System.out.println("Meow! I've marked this task as done:");
@@ -64,7 +64,7 @@ public class Bubu {
         System.out.println(line);
     }
 
-    public void commandUnmark(String input) {
+    private void commandUnmark(String input) {
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         this.tasks.get(index).markAsUndone();
         System.out.println("Meow! I've marked this task as not done yet:");
