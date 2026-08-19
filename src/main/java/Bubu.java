43cad7775cf0d797ca02849fb6a63ec0241bc4f7
Zhou Xinyu <e1525502@u.nus.edu>
2026-08-20 00:27:36 +0800

    Level 0 - Add greeting and exit message

---
+++ src/main/java/Bubu.java
@@ -0,0 +1,13 @@
+public class Bubu {
+    public static void main(String[] args) {
+        String line = "___________________________________________________________";
+        String banner = " /\\___/\\ \n"
+                + "(  o.o  )  Hello! I'm BUBU!\n";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("What can I do for you? Meow!");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon! Meow!");
+        System.out.println(line);
+    }
+}
